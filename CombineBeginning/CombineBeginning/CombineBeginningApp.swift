10038b83ea6e1b4441d5d16a94d16d695630af3f
CMDB-127710 <n.popov@itransition.com>
2022-11-22 15:32:50 +0100

    InitialCommit

---
+++ CombineBeginning/CombineBeginning/CombineBeginningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineBeginningApp.swift
+//  CombineBeginning
+//
+//  Created by CMDB-127710 on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineBeginningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
